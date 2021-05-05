baa66aad84983a9a7c9800fb5e94248a31064019
Pankaj Dwivedi <pankajdwivedi1510@gmail.com>
2021-05-05 14:59:46 +0530

    Finish our Locoframework

---
+++ PDLocoFramework/PDLocoFramework/PDLocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  PDLocoFramework.h
+//  PDLocoFramework
+//
+//  Created by Apple on 05/05/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PDLocoFramework.
+FOUNDATION_EXPORT double PDLocoFrameworkVersionNumber;
+
+//! Project version string for PDLocoFramework.
+FOUNDATION_EXPORT const unsigned char PDLocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDLocoFramework/PublicHeader.h>
+
+
---
+++ PDLocoFramework/PDLocoFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  PDLocoFramework
+//
+//  Created by Apple on 05/05/21.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Some Stuff, here you go hom boy"
+    }
+}
